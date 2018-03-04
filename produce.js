f219d1541d0ba82239973f5dd3e3d517f7874f93
TheConnMan <bcconn2112@gmail.com>
2018-03-04 09:41:13 -0500

    Send groups of messages with the same ID

--- produce.js
+++ produce.js
@@ -6,18 +6,24 @@ const sqs = new AWS.SQS({
 });
 
 const QUEUE_URL = process.env.QUEUE_URL;
+const ids = parseInt(process.env.IDS || '10');
 const count = parseInt(process.env.COUNT || '10');
 
-Array.apply(null, Array(count)).reduce((promise, i) => {
-  return promise.then(() => {
-    const id = uuid();
-    sqs.sendMessage({
-      QueueUrl: QUEUE_URL,
-      MessageBody: JSON.stringify({
-        id
-      }),
-      MessageGroupId: id,
-      MessageDeduplicationId: uuid()
-    }).promise()
+Array(ids).fill().reduce((promise1) => {
+  const id = uuid();
+  return promise1.then(() => {
+    return Array(count).fill().reduce((promise2, i, number) => {
+      return promise2.then(() => {
+        sqs.sendMessage({
+          QueueUrl: QUEUE_URL,
+          MessageBody: JSON.stringify({
+            id,
+            number
+          }),
+          MessageGroupId: id,
+          MessageDeduplicationId: id + number
+        }).promise()
+      });
+    }, Promise.resolve());
   });
 }, Promise.resolve());
